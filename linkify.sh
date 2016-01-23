705f42d032cdae36bef97cb774bfb3fccac7cfaf
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2016-01-24 00:27:19 +0200

    zsh themes and more

--- linkify.sh
+++ linkify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ln -s ~/.vimrc $(pwd)/vim/vimrc
-ln -s ~/.oh-my-zsh/custom/iterm2-ssh.zsh $(pwd)/oh-my-zsh/custom/iterm2-ssh.zsh
+ln -s $(pwd)/vim/vimrc ~/.vimrc
+ln -s $(pwd)/oh-my-zsh/zshrc ~/.zshrc
+ln -s $(pwd)/oh-my-zsh/themes/tatterdemalion.zsh-theme ~/.oh-my-zsh/themes/tatterdemalion.zsh-theme
+ln -s $(pwd)/oh-my-zsh/custom/iterm2-ssh.zsh ~/.oh-my-zsh/custom/iterm2-ssh.zsh
