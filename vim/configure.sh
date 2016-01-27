acfbb485a5ca634567021c4d1eead9832159e553
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2016-01-27 09:00:14 +0200

    create .vim directories

--- vim/configure.sh
+++ vim/configure.sh
@@ -6,6 +6,10 @@ ln -s ~/.vimrc $(pwd)/vim/vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle
 vim +PluginInstall +qall
-pushd ~/.vim/bundle/YouCompleteMe && python install.py --clang-completer
+pushd ~/.vim/bundle/YouCompleteMe && python install.py --clang-completer && popd
+
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+mkdir ~/.vim/backup
 
 
