e1b3dae22551ce7f736dd4faa3e87601ef99b593
macellan <macellan@tatterdemalion.local>
2016-12-14 09:32:37 +0200

    vim and postgres changes

--- vim/configure.sh
+++ vim/configure.sh
@@ -4,7 +4,7 @@ ln -s ~/.vimrc $(pwd)/vim/vimrc
 
 # configure vim
 mkdir -p ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 pushd ~/.vim/bundle/YouCompleteMe && python install.py --clang-completer && popd
 
